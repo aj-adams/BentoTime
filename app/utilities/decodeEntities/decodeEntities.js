177d6c20654a0e8c2812aab94a86466d088e5218
David Drabik <david@daviddrabik.com>
2016-03-15 19:03:13 -0700

    [change] moves decodeEntities docs to Readme

--- app/utilities/decodeEntities/decodeEntities.js
+++ app/utilities/decodeEntities/decodeEntities.js
@@ -1,13 +1,5 @@
-/**
- * Decodes html entities in strings. Uses the browser to decode rather than using
- * a entity database like [he.js](https://github.com/mathiasbynens/he).
- *
- * decodeEntities is a function that has a closure over a new html document so a
- * new one isn't created on each function call.
- * 
- * http://stackoverflow.com/questions/5796718/html-entity-decode/27385169#27385169
- */
 const decodeEntities = (function () {
+  
   //create a new html document (doesn't execute script tags in child elements)
   const doc = document.implementation.createHTMLDocument('');
   const element = doc.createElement('div');
